5c9e43c33055719223419bf009fae5d4c27c911c
anees05 <anees.adams05@gmail.com>
2019-08-03 11:42:08 -0400

    Dislay information in console for users to make purchases and update database

--- bamazon.js
+++ bamazon.js
@@ -33,3 +33,111 @@ function shop() {
     });
 }
 
+var welcome = "\n ===================================\n" +
+    " =====     WELCOME TO BAMAZON   ===== \n"
+
+var goodbye = "\n ===================================n" +
+    " =====     THANKS FOR SHOPPING AT BAMAZON   ===== \n" +
+    " ======  Have a great day!   ===== \n"
+
+function displayProducts() {
+    console.log(welcome);
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id +
+                " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
+        }
+
+        console.log("==================================");
+
+        inquirer.prompt({
+            name: "item_id",
+            message: "What is the ID of the product you would like to purchase?",
+            type: "input",
+            validate: function (value) {
+
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\n Please enter a valid item ID.");
+                    return false;
+                }
+            }
+        }).then(function (answer) {
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: answer.item_id
+            }, function (err, res) {
+                for (var i = 0; i < res.length; i++) {
+                    console.log("Item ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
+                }
+
+                inquirer.prompt({
+                    name: "quant",
+                    message: "How many units are you wanting to buy?",
+                    type: "input",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        } else {
+                            console.log("\n Please enter a valid quantity.");
+                            return false;
+                        }
+                    }
+                }).then(function (answer) {
+                    for (var i = 0; i < res.length; i++) {
+                        var price = res[i].price;
+
+                        var quant = parseInt(res[i].stock_quantity);
+                        var quantNum = parseInt(answer.quant);
+                        var total;
+
+                        if (quant >= quantNum) {
+                            var quantityLeft = quant - quantNum;
+
+                            console.log("Updating stock\n");
+
+                            connection.query("UPDATE products SET ? WHERE ?",
+                                [{
+                                    stock_quantity: quantityLeft
+                                },
+
+                                {
+                                    item_id: res[i].item_id
+                                }
+                                ],
+
+                                function () {
+                                    shop();
+                                }
+
+                            );
+
+                            for (var i = 0; i < res.length; i++) {
+                                total = parseInt(price * quantNum);
+
+                                console.log("Thank You! You bought " + res[i].item_id + " " + res[i].product_name + " for " + "$" + total.toFixed() + " successfully!");
+                            }
+
+                            console.log(goodbye);
+
+                        } else if (quantNum === 0) {
+                            console.log("Please enter an ID");
+                            shop();
+
+                        } else {
+                            console.log("Insufficient quantity!");
+                            shop();
+                        }
+
+                    };
+
+                });
+
+            });
+
+        });
+    });
+};
